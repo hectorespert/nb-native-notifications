395fc3fbbbafc41e3f994be5184de08b7d079fd2
Hector Espert <hectorespertpardo@gmail.com>
2020-07-02 22:33:38 +0200

    Simple linux notifications implementation

---
+++ linux/src/main/java/es/blackleg/nb/notifications/linux/LinuxNotificationDisplayer.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2020 Hector Espert.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.blackleg.nb.notifications.linux;
+
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import es.blackleg.nb.notifications.linux.jna.Libnotify;
+import java.awt.event.ActionListener;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.Icon;
+import javax.swing.JComponent;
+import org.openide.awt.Notification;
+import org.openide.awt.NotificationDisplayer;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Hector Espert
+ */
+@ServiceProvider(service = LinuxNotificationDisplayer.class, position = 50)
+public class LinuxNotificationDisplayer extends NotificationDisplayer {
+
+    private static final Logger LOG = Logger.getLogger(LinuxNotificationDisplayer.class.getName());
+
+    private static final String LIBNOTIFY = "libnotify.so.4";
+
+    private static final String APP_NAME = "netbeans";
+
+    private Libnotify libnotify;
+
+    public LinuxNotificationDisplayer() {
+        try {
+            this.libnotify = Native.load(LIBNOTIFY, Libnotify.class);
+            LOG.log(Level.FINE, "Libnotify library loaded");
+        } catch (UnsatisfiedLinkError unsatisfiedLinkError) {
+            LOG.log(Level.WARNING, "Libnotify library not found", unsatisfiedLinkError);
+        }
+    }
+
+    private boolean isLoaded() {
+        return Objects.nonNull(libnotify);
+    }
+
+    public void start() {
+        if (isLoaded() && !libnotify.notify_is_initted() && libnotify.notify_init(APP_NAME)) {
+            LOG.log(Level.FINE, "Libnotify initted");
+        } else {
+            LOG.log(Level.WARNING, "Unable to start libnotify");
+        }
+    }
+
+    public void stop() {
+        if (isLoaded() && libnotify.notify_is_initted()) {
+            libnotify.notify_uninit();
+            LOG.log(Level.FINE, "Libnotify uninit");
+        }
+    }
+
+    @Override
+    public Notification notify(String title, Icon icon, String detailsText, ActionListener detailsAction, Priority priority) {
+        if (isLoaded() && libnotify.notify_is_initted()) {
+            Pointer notification = libnotify.notify_notification_new(title, detailsText, null);
+            libnotify.notify_notification_show(notification, null);
+        }
+
+        return getDefaultNotificationDisplayer().notify(title, icon, detailsText, detailsAction, priority);
+    }
+
+    @Override
+    public Notification notify(String title, Icon icon, JComponent balloonDetails, JComponent popupDetails, Priority priority) {
+        return getDefaultNotificationDisplayer().notify(title, icon, balloonDetails, popupDetails, priority);
+    }
+    
+    private NotificationDisplayer getDefaultNotificationDisplayer() {
+        return Lookup.getDefault()
+                .lookupAll(NotificationDisplayer.class)
+                .stream()
+                .filter((notificationDisplayer) -> !LinuxNotificationDisplayer.class.isInstance(notificationDisplayer))
+                .map(NotificationDisplayer.class::cast)
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("Default NotificationDisplayer implementation not available"));
+    }
+
+
+}
